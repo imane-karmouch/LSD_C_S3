9b1db2252ce65d165b342b986d006623454e2024
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2022-01-23 02:23:23 +0100

    Create code.c

---
+++ TPoptional/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char* get_next_line(int fd, int x){
+	char* temp;
+	
+	if (read(fd, *temp,x)==-1)
+		EXIT_FAILURE;
+	if (read(fd, *temp,x)==0)
+		printf("file is empty");
+	while(read(fd, *temp,x) WWW!$£	
+
+}
+main(){
+	int fd;
+	fd =open("file.txt", 0_RDONLY)) // get the file descriptor 
+
+	return 0;
+}
