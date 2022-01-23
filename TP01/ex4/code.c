c491d6b7fc06433829f63612239fa45c062eddac
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2022-01-23 02:40:58 +0100

    Update and rename main.c to code.c

---
+++ TP01/ex4/code.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+void print_bits(unsigned char byte){
+unsigned char m;
+for(int i=0;i<8;i++){
+	if (byte &(1<<7){
+	
+	}
+}
+}
+int main(){
+
+	return 0;
+}
