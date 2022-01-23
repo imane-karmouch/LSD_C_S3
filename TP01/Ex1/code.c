ceeb684f3ccae05210bab665a29880b5051c1c9d
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2022-01-23 02:44:04 +0100

    Create code.c

---
+++ TP01/Ex1/code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+void int_to_bin(int num){
+	int msb;
+	(num  31 & msb)?(printf("msb is set\n"):printf("msb is not set\n");
+}
+int main(){
+  check_msb(2);
+	return 0;
+}
